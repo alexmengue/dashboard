57e251c855ec6d9f662442e38b977874c36187cd
Alex <alexmengue1990@hotmail.com>
2023-09-04 19:12:55 -0300

    added a toggle button to show or hide the sidebar menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,10 @@ allSideMenu.forEach(item => {
         li.classList.add('active');
     });
 });
+
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', () => {
+    sidebar.classList.toggle('hide');
+});
